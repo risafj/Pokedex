f3b15edfa3725c7ae5975ad39adfe1f00f66ad96
Risa Fujii <risaf1231@gmail.com>
2020-05-09 17:36:43 +0900

    Search for pokemon using the search bar

--- Pokedex/PokemonListViewController.swift
+++ Pokedex/PokemonListViewController.swift
@@ -1,7 +1,10 @@
 import UIKit
 
-class PokemonListViewController: UITableViewController {
+class PokemonListViewController: UITableViewController, UISearchBarDelegate {
+    // This contains all Pokemon data from the API call.
     var pokemon: [PokemonListResult] = []
+    // This contains just the ones to be displayed.
+    var displayPokemon: [PokemonListResult] = []
     
     @IBOutlet var searchBar: UISearchBar!
 
@@ -11,19 +14,25 @@ class PokemonListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // Setting searchBar's delegate property to this controller.
+        searchBar.delegate = self
+
         guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit=151") else {
             return
         }
-        
+
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else {
                 return
             }
-            
+
             do {
                 let entries = try JSONDecoder().decode(PokemonListResults.self, from: data)
                 self.pokemon = entries.results
+                // Display all pokemon initially.
+                self.displayPokemon = self.pokemon
+
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
@@ -39,12 +48,12 @@ class PokemonListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pokemon.count
+        return displayPokemon.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell", for: indexPath)
-        cell.textLabel?.text = capitalize(text: pokemon[indexPath.row].name)
+        cell.textLabel?.text = capitalize(text: displayPokemon[indexPath.row].name)
         return cell
     }
     
@@ -54,5 +63,21 @@ class PokemonListViewController: UITableViewController {
                 let index = tableView.indexPathForSelectedRow?.row {
             destination.url = pokemon[index].url
         }
+    }
+
+    // Called whenever the user changes the text in the search bar (including when cleared).
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            displayPokemon = pokemon
+            return
+        }
+
+        displayPokemon = []
+        for p in pokemon {
+            if p.name.contains(searchText.lowercased()) {
+                displayPokemon.append(p)
+            }
+        }
+        tableView.reloadData()
     }
 }
